fc45b352f6f1ade854b26be008a3e9e9372c65f2
romi-devx <romi_maximus@hotmail.com>
2020-09-17 17:09:43 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+//COMPONENTS
+import Test from './Test';
+
+function App() {
+	return (
+		<div className="app">
+			<h1>My React App</h1>
+			<Test/>
+		</div>
+	)
+}
+
+export default App;
